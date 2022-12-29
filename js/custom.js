2e6fecb47f55a559734b4a7a8dae9cc41a512c25
Abbhishek Soni <abbhisheksoni001@gmail.com>
2022-12-29 18:21:55 +0530

    code added

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+// Password Input Field Right Eye
+
+$('.pass-eye-img-link').on('click', function(){
+    $('.pass-eye-img').toggleClass('d-none');
+    $('.pass-eye-img-2').toggleClass('d-none');
+})
