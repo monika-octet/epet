6e85c5e15ee532a55542e484a23dd0be8a11a7bd
Abbhishek Soni <abbhisheksoni001@gmail.com>
2022-12-30 17:46:13 +0530

    mobile responsive changes

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,7 @@ $('.pass-eye-img-link').on('click', function(){
     $('.pass-eye-img').toggleClass('d-none');
     $('.pass-eye-img-2').toggleClass('d-none');
 })
+
+$('.show-table').on('click', function(){
+    $('.notes-table').toggleClass('d-none');
+})
