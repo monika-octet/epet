023e014ffc74e65b0be118664d3032db44e8d4e8
Abbhishek Soni <abbhisheksoni001@gmail.com>
2023-01-04 18:11:52 +0530

    qc

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,13 @@ $('.show-table').on('click', function(){
     $('.notes-table').toggleClass('d-none');
     $('.arrow-rotate').toggleClass('rotate');
 })
+
+$('.show-table-1').on('click', function(){
+    $('.notes-table-1').toggleClass('d-none');
+    $('.arrow-rotate-1').toggleClass('rotate');
+})
+
+$('.show-table-2').on('click', function(){
+    $('.notes-table-2').toggleClass('d-none');
+    $('.arrow-rotate-2').toggleClass('rotate');
+})
